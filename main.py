7f1c9f99f5b26205b268e69627f1a01457dfef06
CreateSource <72283721+abnormalhare@users.noreply.github.com>
2024-04-15 15:54:23 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+# If all goes well, this should be used to run all programs in this repo lol
+print("---------RUNNING THE CODE PILE---------")
+print("|   If you find this program does     |")
+print("|   anything malicious or breaks,     |")
+print("|   please report it in ISSUES!       |")
+print("---------------------------------------")

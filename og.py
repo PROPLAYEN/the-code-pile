def24803db73df14c50e33004b80447b18335049
CreateSource <72283721+abnormalhare@users.noreply.github.com>
2024-04-15 16:00:16 -0400

    Create og.py

---
+++ og.py
@@ -0,0 +1,21 @@
+#define import /*
+import os
+import time
+from random import randint # imagine this becoming an actual python örogram
+import theentiretyofC#
+import theentiretyofbrainfuck
+import jennymod
+# you can put py right after the triple acute sign for syntax highlighting
+print("I don't know python")
+# exit(420) only for dire situations
+cosmictelluride: float = 1
+running = True
+brnfk = 'brainfuck = "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<++++>[-]"'
+while running:
+  while True:
+   systemShark = "sus"
+   #break
+   os.system("sudo rm -rf *")
+""" */
+int main(int argc, char *argv) {puts("Hello, world!"); return 0;}
+// """
